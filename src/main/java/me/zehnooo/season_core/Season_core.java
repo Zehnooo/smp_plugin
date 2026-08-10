de26d820428c82d16f499dc7a01a392946ad72a0
Zehnooo <126801501+Zehnooo@users.noreply.github.com>
2026-08-10 17:40:26 -0500

    init

---
+++ src/main/java/me/zehnooo/season_core/Season_core.java
@@ -0,0 +1,16 @@
+package me.zehnooo.season_core;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Season_core extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
