1c5dad9d3b44ac1f894b43d065153d203c5c51c1
Zehnooo <126801501+Zehnooo@users.noreply.github.com>
2026-08-10 21:10:11 -0500

    added season command

---
+++ src/main/java/me/zehnooo/season_core/command/SeasonCommand.java
@@ -0,0 +1,28 @@
+package me.zehnooo.season_core.command;
+
+import me.zehnooo.season_core.season.SeasonManager;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class SeasonCommand implements CommandExecutor {
+
+    private final SeasonManager seasonManager;
+
+    public SeasonCommand(SeasonManager seasonManager) {
+        this.seasonManager = seasonManager;
+    }
+
+    @Override
+    public boolean onCommand(
+            CommandSender sender,
+            Command command,
+            String label,
+            String[] args
+    ) {
+        sender.sendMessage("KirkCraft Season 1");
+        sender.sendMessage("Nether Locked: " + seasonManager.isNetherLocked());
+        sender.sendMessage("End Locked: " + seasonManager.isEndLocked());
+        return true;
+    }
+}
