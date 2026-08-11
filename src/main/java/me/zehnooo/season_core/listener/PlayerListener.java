83ac5d969767f71ee0267fe565dffb7a20818a57
Zehnooo <126801501+Zehnooo@users.noreply.github.com>
2026-08-10 19:03:29 -0500

    added block break event

--- src/main/java/me/zehnooo/season_core/listener/PlayerListener.java
+++ src/main/java/me/zehnooo/season_core/listener/PlayerListener.java
@@ -3,7 +3,9 @@ package me.zehnooo.season_core.listener;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 
 public final class PlayerListener implements Listener {
 
@@ -11,5 +13,11 @@ public final class PlayerListener implements Listener {
     public void onPlayerJoin(PlayerJoinEvent event){
         Player player = event.getPlayer();
         player.sendMessage("Welcome to Season 1 of KirkCraft!!");
+    }
+
+    @EventHandler
+    public void onBlockBreak(BlockBreakEvent event){
+        Player player = event.getPlayer();
+        player.sendMessage("You broke " + event.getBlock().getType().toString());
     }
 }
