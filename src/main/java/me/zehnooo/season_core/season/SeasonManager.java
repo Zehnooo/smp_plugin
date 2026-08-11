53e6f9d2e8956d04497fc998cae0945a16803242
Zehno <126801501+Zehnooo@users.noreply.github.com>
2026-08-11 11:36:03 -0500

    Added methods to check for season end and days until the season ends

--- src/main/java/me/zehnooo/season_core/season/SeasonManager.java
+++ src/main/java/me/zehnooo/season_core/season/SeasonManager.java
@@ -5,16 +5,18 @@ import java.time.Duration;
 
 public final class SeasonManager {
 
-    private static final long NETHER_UNLOCK_TIME = 8;
-    private static final long END_UNLOCK_TIME = 22;
+    private static final long NETHER_UNLOCK_DAY = 8;
+    private static final long END_UNLOCK_DAY = 22;
+    private static final long SEASON_END_DAY = 60;
+
     private final Instant seasonStart = Instant.parse("2026-08-11T15:00:00Z");
 
     public boolean isNetherLocked() {
-        return getSeasonDay() < NETHER_UNLOCK_TIME;
+        return getSeasonDay() < NETHER_UNLOCK_DAY;
     }
 
     public boolean isEndLocked() {
-        return getSeasonDay() < END_UNLOCK_TIME;
+        return getSeasonDay() < END_UNLOCK_DAY;
     }
 
     public long getSeasonDay(){
@@ -22,17 +24,25 @@ public final class SeasonManager {
     }
 
     public long daysUntilNetherUnlock(){
-        return Math.max(0, NETHER_UNLOCK_TIME - getSeasonDay());
+        return Math.max(0, NETHER_UNLOCK_DAY - getSeasonDay());
     }
 
     public long daysUntilEndUnlock(){
-        return Math.max(0,END_UNLOCK_TIME - getSeasonDay());
+        return Math.max(0,END_UNLOCK_DAY - getSeasonDay());
     }
 
     public String getSeasonPhase(){
         if (isNetherLocked()) return "Overworld Age";
         if (isEndLocked()) return "The Underworld";
         return "The End...";
+    }
+
+    public boolean isSeasonOver(){
+        return getSeasonDay() >= SEASON_END_DAY;
+    }
+
+    public long daysUntilSeasonEnd(){
+        return Math.max(0, SEASON_END_DAY - getSeasonDay());
     }
 
 }
