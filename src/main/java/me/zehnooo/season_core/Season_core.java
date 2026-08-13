87b7baf78e8e89e539283b4e8954757d4c70ad80
Zehno <126801501+Zehnooo@users.noreply.github.com>
2026-08-13 12:10:44 -0500

    forgot to enable relic listener

--- src/main/java/me/zehnooo/season_core/Season_core.java
+++ src/main/java/me/zehnooo/season_core/Season_core.java
@@ -3,6 +3,7 @@ package me.zehnooo.season_core;
 import me.zehnooo.season_core.announcement.AnnouncementService;
 import me.zehnooo.season_core.command.SeasonCommand;
 import me.zehnooo.season_core.listener.PlayerListener;
+import me.zehnooo.season_core.listener.RelicListener;
 import me.zehnooo.season_core.relic.RelicManager;
 import me.zehnooo.season_core.season.SeasonConfigManager;
 import me.zehnooo.season_core.season.SeasonManager;
@@ -32,6 +33,8 @@ public final class Season_core extends JavaPlugin {
         RelicManager relicManager = new RelicManager(this);
         SeasonCommand seasonCommand = new SeasonCommand(seasonManager, relicManager);
         Objects.requireNonNull(getCommand("season")).setExecutor(seasonCommand);
+
+        RelicListener relicListener = new RelicListener(relicManager);
 
         AnnouncementService announcementService = new AnnouncementService(seasonManager, dataManager);
         getServer().getScheduler().runTaskTimer(this, announcementService::check, 20L, 20L * 60 * 60);
