5879e5e2917b51547dc1cf0c32a7de0f6fb7b681
Zehno <126801501+Zehnooo@users.noreply.github.com>
2026-08-13 12:04:53 -0500

    added test for relic right click

---
+++ src/main/java/me/zehnooo/season_core/listener/RelicListener.java
@@ -0,0 +1,32 @@
+package me.zehnooo.season_core.listener;
+
+import me.zehnooo.season_core.relic.RelicManager;
+import me.zehnooo.season_core.relic.RelicType;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.Listener;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
+
+
+public class RelicListener implements Listener {
+    private final RelicManager relicManager;
+
+    public RelicListener(RelicManager relicManager) {
+        this.relicManager = relicManager;
+    }
+
+    @EventHandler
+    public void onRightClick(PlayerInteractEvent event) {
+        Action action = event.getAction();
+        if (action != Action.RIGHT_CLICK_AIR && action != Action.RIGHT_CLICK_BLOCK) return;
+        Player player = event.getPlayer();
+        ItemStack hand = player.getInventory().getItemInMainHand();
+        if (!relicManager.isRelic(hand)) return;
+        player.sendMessage(relicManager.getType(hand).toString());
+    }
+}
--- src/main/java/me/zehnooo/season_core/relic/RelicType.java
+++ src/main/java/me/zehnooo/season_core/relic/RelicType.java
@@ -1,6 +1,7 @@
 package me.zehnooo.season_core.relic;
 
 import org.bukkit.Material;
+import org.bukkit.potion.PotionEffectType;
 
 public enum RelicType {
 
@@ -9,7 +10,7 @@ public enum RelicType {
     NAME(Material.ITEM, "")
     Emberblade - Right Click gives fire resistance for 6 seconds. Successful crit applies fire aspect to hit entity for 3 seconds
     Windspear - Right click grants user speed 2 for 6 seconds
-    Axe of Crippling - Successful crit applies slowness to hit entity for 4 seconds
+    Axe of Crippling - Successful crit applies weakness to hit entity for 3 seconds
     Phase Bow - Successful hit teleports user to target
     Horn of Health - Gives regen to user for 5 seconds
     The Ward - Blocking gives resistance for 6 seconds
@@ -17,21 +18,27 @@ public enum RelicType {
     Cloak - Gives user invisibility for 8 seconds. Attacking or taking damage removes invis
      */
 
-    EMBER_BLADE(Material.NETHERITE_SWORD, "Ember Blade"),
-    WIND_SPEAR(Material.NETHERITE_SPEAR, "Wind Spear"),
-    AXE_OF_CRIPPLING(Material.NETHERITE_AXE, "Axe of Crippling"),
-    PHASE_BOW(Material.BOW, "Phase Bow"),
-    HORN_OF_HEALTH(Material.GOAT_HORN, "Horn of Health"),
-    THE_WARD(Material.SHIELD, "The Ward"),
-    FROST_SHARD(Material.AMETHYST_SHARD, "Frost Shard"),
-    CLOAK(Material.ECHO_SHARD, "Cloak");
+    EMBER_BLADE(Material.NETHERITE_SWORD, "Ember Blade", PotionEffectType.FIRE_RESISTANCE, 20 * 6, 1),
+    WIND_SPEAR(Material.NETHERITE_SPEAR, "Wind Spear", PotionEffectType.SPEED, 20 * 6, 1),
+    WAR_AXE(Material.NETHERITE_AXE, "War Axe", PotionEffectType.WEAKNESS, 20 * 3, 1),
+    PHASE_BOW(Material.BOW, "Phase Bow", null, 0, 0),
+    HORN_OF_HEALTH(Material.GOAT_HORN, "Horn of Health", PotionEffectType.REGENERATION, 20 * 5, 1),
+    THE_WARD(Material.SHIELD, "The Ward", PotionEffectType.RESISTANCE, 20 * 6, 1),
+    SLOWNESS_SHARD(Material.AMETHYST_SHARD, "Slowness Shard", PotionEffectType.SLOWNESS, 20 * 3, 1),
+    CLOAK(Material.ECHO_SHARD, "Cloak", PotionEffectType.INVISIBILITY, 20 * 8, 0);
 
     private final Material material;
     private final String displayName;
+    private final PotionEffectType effect;
+    private final int duration;
+    private final int amplifier;
 
-    RelicType(Material material, String displayName) {
+    RelicType(Material material, String displayName, PotionEffectType effect, int duration, int amplifier) {
         this.material = material;
         this.displayName = displayName;
+        this.effect = effect;
+        this.duration = duration;
+        this.amplifier = amplifier;
     }
 
     public Material material() {
@@ -41,5 +48,18 @@ public enum RelicType {
     public String displayName() {
         return displayName;
     }
+
+    public PotionEffectType effect() {
+        return effect;
+    }
+
+    public int duration() {
+        return duration;
+    }
+
+    public int amplifier() {
+        return amplifier;
+    }
+
 
 }
